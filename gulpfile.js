238872edb4b9d33455bf2c4be015374620a98518
Jamie Mason <jamie@foldleft.io>
2016-10-17 15:11:56 +0100

    [WIP] convert to typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,49 @@
-// 3rd party modules
 var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
+var tsify = require('tsify');
 
-// public
-gulp.task('browserify', function () {
-  browserify('./src/index.js')
+gulp.task('default', function() {
+  return browserify({
+      basedir: '.',
+      debug: true,
+      entries: ['index.ts'],
+      cache: {},
+      packageCache: {}
+    })
+    .plugin(tsify)
     .bundle()
-    .pipe(source('jasmine-matchers.js'))
-    .pipe(gulp.dest('dist'));
-  browserify('./test/index.js')
-    .bundle()
-    .pipe(source('jasmine-matchers.spec.js'))
-    .pipe(gulp.dest('dist'));
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('built'));
 });
 
-gulp.task('build', [
-  'browserify'
-]);
+// // 3rd party modules
+// var browserify = require('browserify');
+// var gulp = require('gulp');
+// var source = require('vinyl-source-stream');
 
-gulp.task('development-watchers', ['build'], function () {
-  return gulp.watch([
-    '*.js',
-    'src/**/*.js',
-    'test/**/*.js'
-  ], [
-    'build'
-  ]);
-});
+// // public
+// gulp.task('browserify', function () {
+//   browserify('./src/index.js')
+//     .bundle()
+//     .pipe(source('jasmine-matchers.js'))
+//     .pipe(gulp.dest('dist'));
+//   browserify('./test/index.js')
+//     .bundle()
+//     .pipe(source('jasmine-matchers.spec.js'))
+//     .pipe(gulp.dest('dist'));
+// });
+
+// gulp.task('build', [
+//   'browserify'
+// ]);
+
+// gulp.task('development-watchers', ['build'], function () {
+//   return gulp.watch([
+//     '*.js',
+//     'src/**/*.js',
+//     'test/**/*.js'
+//   ], [
+//     'build'
+//   ]);
+// });
